1cedb03a3114af5291ba327dfc6f20a4289c5307
Sammy Tam <sammytam2005@hotmail.com>
2016-10-14 05:37:55 +0000

    Stay login after registeraion

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,6 +8,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
 
     if @user.save
+      session[:user_id] = @user.id
       redirect_to [:products], notice: 'User created!'
     else
       render :new
