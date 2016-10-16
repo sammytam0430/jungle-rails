a8f9305396bb677b662d2d23f8c7a9582e0f72d6
Sammy Tam <sammytam2005@hotmail.com>
2016-10-16 17:34:32 +0000

    refactored session route path

--- config/routes.rb
+++ config/routes.rb
@@ -10,9 +10,9 @@ Rails.application.routes.draw do
 
   resources :users, only: [:new, :create]
 
-  get '/login' => 'sessions#new'
-  post '/login' => 'sessions#create'
-  get '/logout' => 'sessions#destroy'
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  get 'logout', to: 'sessions#destroy'
 
   resource :cart, only: [:show] do
     put    :add_item
